bb7955e136fd40a87188763c10d1f5a2ee4d210f
Nida Naz <162494835+NidaNaz804@users.noreply.github.com>
2025-02-04 09:54:30 +0500

    initial commit

---
+++ src/app/type.d.ts
@@ -0,0 +1,15 @@
+type Blog ={
+    Title: string,
+    Paragraph: string,
+    Image: any,
+    slug: string,
+    block:String
+
+}
+
+// declare namespace JSX {
+//     interface IntrinsicElements {
+//       commentBox: any;
+//     }
+//   }
+  
